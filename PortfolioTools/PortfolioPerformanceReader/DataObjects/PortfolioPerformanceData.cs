4683f459b2511f92b5637fb143cab2d99a18a66c
Markus Liebl <Medlan@gmx.net>
2022-01-12 21:15:41 +0100

    Initial Commit

---
+++ PortfolioTools/PortfolioPerformanceReader/DataObjects/PortfolioPerformanceData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioPerformanceReader.DataObjects
+{
+    public class PortfolioPerformanceData
+    {
+        public List<Security> Securities { get; set; }
+
+        public PortfolioPerformanceData()
+        {
+            // Init default values & lists
+            Securities = new List<Security>();
+        }
+    }
+}
---
+++ PortfolioTools/PortfolioPerformanceReader/DataObjects/Security.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioPerformanceReader.DataObjects
+{
+    public class Security
+    {
+        public string Uuid { get; set; } = "";
+        public int ID { get; set; }
+        public string Name { get; set; } = "";
+        public DateTime LastUpdate { get; set; }
+        public long LastValue { get; set; }
+        public long Shares { get; set; }
+        public string ISIN { get; set; } = "";
+        public string WKN { get; set; } = "";
+        public string TickerSymbol { get; set; } = "";
+        public string Feed { get; set; } = "";
+
+        
+    }
+}
