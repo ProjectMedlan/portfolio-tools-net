d37017cbe62f272e5746f5bc1bc2f19f8f533afd
Markus Liebl <Medlan@gmx.net>
2022-01-15 15:46:50 +0100

    Added first unit tests

--- test/PortfolioPerformanceReaderTest/PPReaderTestClass.cs
+++ test/PortfolioPerformanceReaderTest/PPReaderTestClass.cs
@@ -1,7 +1,27 @@
-﻿namespace PortfolioPerformanceReaderTests
+﻿using PortfolioPerformanceReader;
+using Xunit;
+
+namespace PortfolioPerformanceReaderTests
 {
     public class PPReaderTestClass
     {
+        const string localPPFile = @"";
+
+        [Theory]
+        [InlineData(localPPFile)]
+        public async void FileReadNoExceptionImplicit(string file)
+        {
+            // Brad Wilson describes it on github: Think of it this way: every line of code you write outside of
+            // a try block has an invisible Assert.DoesNotThrow around it.
+            await PortfolioPerformanceDataReader.ReadPortfolioPerformanceFile(file);
+        }
+        [Theory]
+        [InlineData(localPPFile)]
+        public async void FileReadNoExceptionExplicit(string file)
+        {
+            var exception = await Record.ExceptionAsync(async () => await PortfolioPerformanceDataReader.ReadPortfolioPerformanceFile(file));
+            Assert.Null(exception);
+        }
 
     }
 }
