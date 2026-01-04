7042347f1c80566b9c68befe24267145f936fd37
ProjectMedlan <ProjectMedlan@gmail.com>
2026-01-04 11:04:17 +0100

    .NET 10 Moved 'Bestandsliste' to sample folder

---
+++ sample/Bestandsliste/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PortfolioPerformance.DataObjects;
+using System.Diagnostics;
+
+PortfolioPerformanceData data = await PortfolioPerformance.Reader.PortfolioPerformanceDataReader.ReadPortfolioPerformanceFile(@"D:\Daten\Dokumente\Banken & Finanzen\Portfolio Performance\medlan_alles.xml");
+data.Securities.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+foreach (var item in data.Securities)
+{
+    Debug.WriteLine($"{item.ISIN};{item.Shares / 1000.0 / 1000};{item.Name}");
+}
